8ff72d0639916a2f11f3ef09e6034b13448b406b
vasich <vass.vass@mail.ru>
2019-01-02 20:01:48 +0300

    tr

--- Constants.py
+++ Constants.py
@@ -13,17 +13,16 @@ StickerPackIGPlus = [
     'CAADAgADEAADLS6WDmmATLm0uRW_Ag']  # щупак
 
 StickerPackIGPlusComments = {
-    'CAADAgADAwADLS6WDqoityDowUMrAg': 'U+1F60D',  # шурик
-    'CAADAgADBAADLS6WDrXagHTFRERLAg': 'присядь на что-нибудь, не ссы',  # хоба
-    'CAADAgADBQADLS6WDneo_ua5k91SAg': 'только не беси меня',  # кузнецова
-    'CAADAgADBgADLS6WDlz7SX2WcFZPAg': '',  # шестопалова
+    'CAADAgADAwADLS6WDqoityDowUMrAg': '❤',  # шурик
+    'CAADAgADBAADLS6WDrXagHTFRERLAg': 'присядь на что-нибудь,сто раз так делала',  # хоба
+    'CAADAgADBQADLS6WDneo_ua5k91SAg': 'не беси меня',  # кузнецова
+    'CAADAgADBgADLS6WDlz7SX2WcFZPAg': 'Ай шуд тэйк а шауэ энд вош май хэйр',  # шестопалова
     'CAADAgADBwADLS6WDo1d3AQuzCiKAg': 'тэйк ит изи',  # кондра
-    'CAADAgADCAADLS6WDs1hqy0XMmErAg': '',  # дерюгина
-    'CAADAgADCQADLS6WDsTytcbeP52MAg': '',  # тягунов
-    'CAADAgADCgADLS6WDqAif6Bk1y2vAg': '',  # дед мороз
-    'CAADAgADCwADLS6WDlFKm4VgKPp2Ag': 'йоу',  # фил
+    'CAADAgADCAADLS6WDs1hqy0XMmErAg': 'у меня лапки',  # дерюгина
+    'CAADAgADCQADLS6WDsTytcbeP52MAg': 'и чё',  # тягунов
+    'CAADAgADCgADLS6WDqAif6Bk1y2vAg': 'застегнись!',  # дед мороз
+    'CAADAgADCwADLS6WDlFKm4VgKPp2Ag': 'йоу🤛',  # фил
     'CAADAgADDgADLS6WDp9ZO0rMGs-FAg': 'не ной',  # грусть
-    'CAADAgADDwADLS6WDkJi7oP-oaMzAg': '',  # я
-    'CAADAgADEAADLS6WDmmATLm0uRW_Ag': 'Щупак Щупака видит издалека. и рисует '
-
+    'CAADAgADDwADLS6WDkJi7oP-oaMzAg': 'тупой мужик',  # я
+    'CAADAgADEAADLS6WDmmATLm0uRW_Ag': 'Щупак Щупака видит издалека'
 }
